66d950892a14486029c7b54661b85bc340befa24
makomikro <106661022+makomikro@users.noreply.github.com>
2023-12-07 20:57:52 +0100

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,11 @@
+def lovac_grafika(t1,t2):
+    
+    lik=pygame.draw.rect(sur_obj, (255,100,255), (t1, t2, 10, 10))
+    
+    return lik
+
+def lik_grafika(p1,p2):
+    
+    lik=pygame.draw.rect(sur_obj, (255,0,0), (p1, p2, 10, 10))
+    
+    return lik
